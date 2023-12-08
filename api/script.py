22bb2fadef3707f4cba1ab079218654ef3021ac9
n-kmron <cameronnoupoue@gmail.com>
2023-12-08 10:11:22 +0100

    première version du service web

--- api/script.py
+++ api/script.py
@@ -1,5 +1,25 @@
 import xmlrpc.client
 
+def search_book_by_name(models, db, uid, password):
+    # Demander à l'utilisateur le nom du livre à rechercher
+    book_name = input("Entrez le nom du livre (titre) à rechercher : ")
+
+    # Recherche des livres avec le nom spécifié
+    search_domain = [('name', 'ilike', book_name)]
+    books = models.execute_kw(db, uid, password,
+                              'esi.book', 'search_read', [search_domain])
+
+    # Afficher les résultats de la recherche
+    if books:
+        print(f"Résultats de la recherche pour '{book_name}':")
+        for book in books:
+            print(" − ID : ", book.get("id"))
+            print(" − Nom : ", book.get("name"))
+            print(" − Pages : ", book.get("pages"))
+            print(" − Date de création : ", book.get("date_created"))
+    else:
+        print("Aucun livre trouvé avec le nom '{book_name}'.")
+
 # Paramètres de connexion
 url = "http://localhost:8069"
 db = "dev01"
@@ -22,7 +42,6 @@ models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
 hasRight = models.execute_kw(db, uid, password,
                              'esi.book', 'check_access_rights',
                              ['read'], {'raise_exception': False})
-print("Droit de lecture sur le esi.book : ", hasRight)
 
 books = models.execute_kw(db, uid, password,
                           'esi.book', 'search_read', [[]])
@@ -49,4 +68,3 @@ print("Livre supprimé via l'API avec l'id : ", id_created)
 
 
 #https://www.odoo.com/documentation/14.0/fr/developer/reference/external_api.html
-#se connecter alternativement avec la clé d'API
